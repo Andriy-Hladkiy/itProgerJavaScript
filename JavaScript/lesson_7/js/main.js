2384506c94a4c8df4b3614a2cc8eff3315193072
Andrii <andriyha98@gmail.com>
2025-10-22 16:51:38 +0200

    add lesson 7

---
+++ JavaScript/lesson_7/js/main.js
@@ -0,0 +1,12 @@
+var arr = [5, true, "string", 5.7, 0, -100];
+arr[3] = "word"
+console.log(arr[3]);
+console.log(arr.length);
+
+
+var matrix = [
+  [4, 6, 8], ["string", 5.7], [0, -100]
+];
+
+matrix[1][0] = 90;
+console.log(matrix);
