74566bae94d9100074767d88917afe9d30e37a26
Andrii <andriyha98@gmail.com>
2025-10-23 16:05:19 +0200

    new lesson 8

--- JavaScript/lesson_8/js/main.js
+++ JavaScript/lesson_8/js/main.js
@@ -2,8 +2,46 @@
 //   console.log(i)
 // }
 
-var j = 0;
+// var j = 0;
+// while (j < 10) {
+//   console.log(j);
+//   j++
+// }
+//
+//
+// var isHasCar = true;
+//
+// while (isHasCar) {
+//
+// }
 
-while (j < 10) {
 
+// var x = 0;
+//
+// do {
+//   console.log(x);
+//   x++
+// }while(x < 10);
+
+// for(var i = 10; i <= 20; i += 2) {
+//   if (i > 15)
+//     break;
+//
+//   console.log(i)
+// }
+
+// for(var i = 10; i <= 20; i++) {
+//   if (i % 2 == 0){
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+
+var arr = [5, 7, 3, 8, 9, 91]
+
+for (var i = 0; i < arr.length; i++) {
+  arr[i] *= 2
+  console.log("Element " + (i + 1) + ": " + arr[i]);
 }
+
