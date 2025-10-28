9fb080d2a560849af04728da25ad630209445c2b
Andrii <andriyha98@gmail.com>
2025-10-28 10:00:23 +0100

    added lesson 11

--- JavaScript/lesson_11/js/main.js
+++ JavaScript/lesson_11/js/main.js
@@ -1,3 +1,23 @@
-function onClickButton() {
-  alert("You clicked the button :)");
+// function onClickButton() {
+//   alert("You clicked the button :)");
+// }
+
+var counter = 0;
+
+function onClickButton(el) {
+  counter++;
+  el.innerHTML = "You clicked the button: " + counter;
+  el.style.background = "red";
+  el.style.color = "blue";
+
+  el.style.cssText = "border-radius: 5px; border: 0; font-size: 20px";
+  // console.log(el.name);
+}
+
+
+function onInput(el) {
+  if (el.value == "Hello") {
+    alert("And you Hello")
+  }
+  console.log(el.value);
 }
